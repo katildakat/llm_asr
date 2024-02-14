304b43fdfd97cd689cc0a053ba01f0e2b9dfbea7
katildakat <homeisthesea@gmail.com>
2024-02-14 12:50:54 +0200

    added feature extraction to the collator

--- data.py
+++ data.py
@@ -2,17 +2,18 @@ from transformers import DataCollatorWithPadding
 import torch
 
 class MyDataCollator:
-    def __init__(self, tokenizer):
+    def __init__(self, tokenizer, feature_extractor):
       self.tokenizer = tokenizer
       self.tokenizer.pad_token = tokenizer.eos_token
       self.tokenizer.add_eos_token = False
       self.tokenizer.add_bos_token = False
+      self.feature_extractor = feature_extractor
       self.data_collator = DataCollatorWithPadding(tokenizer, return_tensors="pt")
 
     def __call__(self, features):
-      # batch input_features directly
-      batched_input_features = torch.stack([f['input_features'].squeeze() for f in features])
-
+      # extract features
+      batched_input_features = self.feature_extractor([f['audio_array'] for f in features], sampling_rate=16000,  return_tensors="pt")
+     
       # tokenize texts
       # ensure that eos token is always added
       tokenized_texts = [self.tokenizer(f['text'] + self.tokenizer.eos_token) for f in features]
